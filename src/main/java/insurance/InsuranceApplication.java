176cf566f1443345b84a0a3352a10b3b7be6fa27
Mahsa <mahsa.mohseni@snapppay.ir>
2025-12-28 21:18:32 +0330

    Remove timezone

--- src/main/java/insurance/InsuranceApplication.java
+++ src/main/java/insurance/InsuranceApplication.java
@@ -5,17 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-import java.util.TimeZone;
-
 
 @SpringBootApplication
 @Slf4j
 @EnableCaching
 public class InsuranceApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
 
-        TimeZone.setDefault(TimeZone.getTimeZone("GMT+03:30"));
 
         SpringApplication.run(InsuranceApplication.class, args);
     }
